e853d2e727a348e1a0e5d49e8e9bb2a5202817ac
Martin Mroc <mato.mroc@gmail.com>
2019-07-12 13:48:50 +0200

    Second commit

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
-	var express = require('express'); 
+	
+    var express = require('express'); 
     var app = express(); 
     var bodyParser = require('body-parser');
     var multer = require('multer');
@@ -52,20 +53,47 @@
             try {
                 exceltojson({
                     input: req.file.path,
-                    output: null, //since we don't need output.json
-                    lowerCaseHeaders:true
+                    output: __dirname + '/test.json', //null - since we don't need output.json
+                    lowerCaseHeaders: true
                 }, function(err,result){
                     if(err) {
                         return res.json({error_code:1,err_desc:err, data: null});
                     } 
-                    res.json({error_code:0,err_desc:null, data: result});
+                     res.json(result);
                 });
             } catch (e){
                 res.json({error_code:1,err_desc:"Corupted excel file"});
             }
+            
         })
        
     });
+
+    const fs = require('fs')
+
+    fs.readFile('./test.json', (err, data) => {
+      if (err) throw err;
+      console.log(data);
+    });
+
+    // const storeData = (data, path) => {
+    //   try {
+    //     fs.writeFileSync('./test.json', JSON.stringify(data))
+    //   } catch (err) {
+    //     console.error(err)
+    //   }
+    // }
+
+    // var result = result();
+
+    // const fs = require('fs');
+    // fs.writeFile("data.json", result, function(err) {
+    // if(err) {
+    //     return console.log(err);
+    // }
+
+    // console.log("The file was saved!");
+    // }); 
 	
     app.use('/css', express.static('css'));
     app.use('/js', express.static('js'));
