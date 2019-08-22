b1f7d4fdd747fb9ca077e6f76e6669aaa881eeb2
unknown <mato.mroc@gmail.com>
2019-08-22 12:00:41 +0200

    zmeny

--- public/index.js
+++ public/index.js
@@ -3,193 +3,15 @@
 
   $ = $ && $.hasOwnProperty('default') ? $['default'] : $;
 
-  function initGraf() {
-    window.chartModel = {};
-    window.uploadedData = {};
-
-    $("#save-btn").click(function() {
-      $("#myChart")
-        .get(0)
-        .toBlob(function(blob) {
-          var subor = $('input[name="subor"]').val();
-          saveAs(blob, subor + ".png");
-        });
-    });
-
-    $('input[type="file"]').change(function(e) {
-      var fileName = e.target.files[0].name;
-      alert('Bol vybratý "' + fileName + '" súbor.');
-    });
-
-    // Global option
-    Chart.defaults.global.defaultFontFamily = "Georgia";
-    Chart.defaults.global.defaultFontSize = 18;
-    Chart.defaults.global.defaultFontColor = "black";
-    Chart.defaults.global.defaultFontStyle = "normal";
-    Chart.defaults.global.responsive = true;
-
-    var ctx = document.getElementById("myChart").getContext("2d");
-    window.chart = new Chart(ctx, {
-      type: "line",
-      data: {
-        labels: [],
-        datasets: [],
-      },
-      options: {
-        responsive: true,
-        title: {
-          display: false,
-          text: "Analýza technologických dát o procese skujňovania",
-          fontSize: 25,
-          fontColor: "blue",
-          fontStyle: "bold",
-          padding: 5,
-        },
-
-        legend: {
-          position: "top",
-          display: true,
-          fontStyle: "bold",
-        },
-
-        layout: {
-          padding: {
-            left: 20,
-            top: 20,
-            left: 0,
-            bottom: 0,
-          },
-        },
-
-        scales: {
-          yAxes: [
-            {
-              gridLines: {
-                display: true,
-                color: "black",
-                borderDash: [2],
-              },
-              ticks: {
-                beginAtZero: true,
-              },
-              scaleLabel: {
-                display: true,
-                labelString: "Hodnoty",
-                fontColor: "black",
-              },
-            },
-          ],
-          xAxes: [
-            {
-              gridLines: {
-                display: true,
-                color: "black",
-                borderDash: [2],
-              },
-              scaleLabel: {
-                display: true,
-                labelString: "Čas(s)",
-                fontColor: "black",
-              },
-              ticks: {
-                beginAtZero: true,
-                min: 0,
-                stepSize: 50,
-              },
-            },
-          ],
-        },
-        pan: {
-          enabled: true,
-          mode: "xy",
-        },
-
-        zoom: {
-          enabled: true,
-          mode: "xy",
-        },
-      },
-    });
-  }
-
-  function renderChart(chartModel) {
-    window.chart.options.legend.display = true;
-    window.chart.options.title.display = true;
-    window.chart.data.datasets = [];
-    window.chart.data.labels = [];
-
-    let maxLength = 0;
-
-    for (let col in chartModel) {
-      window.chart.data.datasets.push({
-        data: chartModel[col].data,
-        label: chartModel[col].label,
-        fill: false,
-        backgroundColor: "transparent",
-        borderColor: chartModel[col].borderColor,
-        borderWidth: 1,
-      });
-
-      if (maxLength < chartModel[col].data.length) maxLength = chartModel[col].data.length;
-    }
-
-    // TODO: prist na to, ako vybrat nie len labels ale aj data
-    for (let i = 0; i < maxLength; i++) {
-      window.chart.data.labels.push(i);
-    }
-
-    // window.chart.data.labels = [0];
-    // let count = 0;
-    // while (count < maxLength) {
-    //   if (count % 25) {
-    //     window.chart.data.labels.push(window.chart.data.labels[i] + 25)
-    // }
-    // for (let i = 0; i < maxLength; i++) {
-    //   window.chart.data.labels.push(window.chart.data.labels[i] + 25)
-    // }
-
-    window.chart.options.scales = {
-      xAxes: [
-        {
-          gridLines: {
-            display: true,
-            color: "black",
-            borderDash: [2],
-          },
-          scaleLabel: {
-            display: true,
-            labelString: "Čas(s)",
-            fontColor: "black",
-          },
-          ticks: {
-            beginAtZero: true,
-            userCallback: function(item, index) {
-              if (!(index % 0.5)) return item;
-            },
-            min: 0,
-            max: 500,
-            stepSize: 50,
-          },
-        },
-      ],
-    };
-
-    window.chart.update();
-  }
+  $('input[type="file"]').change(function(e) {
+    var fileName = e.target.files[0].name;
+    alert('Bol vybratý "' + fileName + '" súbor.');
+  });
 
   var config = {
     frontendUrl: 'http://localhost:3000',
     backendUrl: 'http://localhost:3000'
   };
-
-  function getRandomColor() {
-    var letters = "0123456789ABCDEF";
-    var color = "#";
-    for (var i = 0; i < 6; i++) {
-      color += letters[Math.floor(Math.random() * 16)];
-    }
-    return color;
-  }
 
   function registerUploadFile() {
     $("#but_upload").click(function() {
@@ -205,7 +27,6 @@
       })
         .then(res => res.json())
         .then(response => {
-          console.log(response);
           if (response && response != 0) {
             console.log("response: ", response);
             window.myJSONData = response.data;
@@ -266,66 +87,6 @@
                 }
               }
 
-              window.uploadedData[fileName] = dataObject;
-
-              riadok.find("#add-to-chart").click(function() {
-                let stlpcestring = $("#" + novyRiadok.attr("id"))
-                  .find(".btn.dropdown-toggle.btn-light")
-                  .attr("title");
-                let stlpce = stlpcestring.split(", ");
-
-                for (let i = 0; i < stlpce.length; i++) {
-                  window.chartModel[fileName + "_" + stlpce[i]] = {
-                    data: window.uploadedData[fileName][stlpce[i]],
-                    label: fileName + "_" + stlpce[i],
-                    borderColor: getRandomColor(),
-                  };
-                }
-
-                renderChart(chartModel);
-              });
-
-              riadok.find("#sum_co_co2").click(function() {
-                let stlpcestring = $("#" + novyRiadok.attr("id"))
-                  .find(".btn.dropdown-toggle.btn-light")
-                  .attr("title");
-                let stlpce = stlpcestring.split(", ");
-
-                // for (let i = 0; i < stlpce.length; i++){
-                //     window.chartModel[fileName + "_" + stlpce[i]] =
-                // }
-
-                // let pole1 = [];
-                // let pole2 = [];
-                // let vysledok = [];
-
-                // for(let i = 0; i < stlpce.length; i++){
-                //     window.chartModel[fileName + "_" + stlpce[i]] = pole1.push(stlpce[i].length);
-                //     window.chartModel[fileName + "_" + stlpce[i]] = pole2.push(stlpce[i].length);
-
-                //     pole1 =  array[i].val();
-                // }
-                // for(var i = 0; i < pole1.length; i++){
-                //    vysledok.push(pole1[i] + pole2[i]);
-                // }
-
-                // console.log("Výsledný stĺpec: ", stlpce.length);
-              });
-
-              // Zmazanie z grafu
-              riadok.find("#delete-from-chart").click(function() {
-                let stlpcestring = $("#" + novyRiadok.attr("id"))
-                  .find(".btn.dropdown-toggle.btn-light")
-                  .attr("title");
-                let stlpce = stlpcestring.split(", ");
-
-                for (let i = 0; i < stlpce.length; i++) {
-                  delete window.chartModel[fileName + "_" + stlpce[i]];
-                }
-
-                renderChart(chartModel);
-              });
-
               alert("Súbor bol úspešne nahraný! ", response);
             }
           } else {
@@ -337,6 +98,5 @@
   }
 
   registerUploadFile();
-  initGraf();
 
 }($));
