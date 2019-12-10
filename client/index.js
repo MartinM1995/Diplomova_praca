d13136149e59aa975cb0d2a00631fb56726d72cc
unknown <mato.mroc@gmail.com>
2019-12-10 11:40:37 +0100

    last

--- client/index.js
+++ client/index.js
@@ -3,9 +3,15 @@ import { registerUploadFile } from "./lib/upload";
 import { CURRENT_LANG } from "./language";
 
 $('input[type="file"]').change(function (e) {
-  var fileName = e.target.files[0].name;
+    var lang = localStorage.getItem("lang");
+    var fileName = e.target.files[0].name;
+    if (lang === "sk") {
+        alert("Súbory boli vybraté.");
+    } else {
+        alert("The files was selected.");
+    }
 
-  alert(CURRENT_LANG["fileName"](fileName));
+    // alert(CURRENT_LANG["fileName"](fileName));
 });
 
-registerUploadFile();
+registerUploadFile()
--- client/lib/upload.js
+++ client/lib/upload.js
@@ -3,53 +3,48 @@ import config from "../config";
 
 export function registerUploadFile() {
 
-  $("#but_upload").click(function () {
-    var lang = localStorage.getItem('lang');
-    console.log($("#file")[0])
-    var file = $("#file")[0].files[0];
-    var formData = new FormData();
-    console.log(Object.values($("#file")[0].files))
-    let formDataArray = []
-    Object.values($("#file")[0].files).forEach(file => {
-      formData.append("files", file);
-      // console.log(file, formData)
-      // formDataArray.push(formData)
-    })
-    console.log(formDataArray)
+    $("#but_upload").click(function() {
+        var lang = localStorage.getItem('lang');
+        var file = $("#file")[0].files[0];
+        var formData = new FormData();
+        let formDataArray = [];
+        Object.values($("#file")[0].files).forEach(file => {
+            formData.append("files", file);
+        });
 
-    fetch(`${config.backendUrl}/api/upload`, {
-      method: "POST",
-      mode: "cors",
-      body: formData,
-    })
-      .then(res => res.json())
-      .then(response => {
-        if (response && response != 0) {
-          console.log("response: ", response);
-          window.myJSONData = response.data;
+        fetch(`${config.backendUrl}/api/upload`, {
+                method: "POST",
+                mode: "cors",
+                body: formData,
+            })
+            .then(res => res.json())
+            .then(response => {
+                if (response && response != 0) {
+                    console.log("response: ", response);
+                    window.myJSONData = response.data;
 
-          var fileName = $("input[type=file]")
-            .val()
-            .split("\\")
-            .pop();
-          $("#target").attr("value", "Súbor: " + fileName);
+                    var fileName = $("input[type=file]")
+                        .val()
+                        .split("\\")
+                        .pop();
+                    $("#target").attr("value", "Súbor: " + fileName);
 
-          if (response.data && response.data[0]) {
+                    if (response.data && response.data[0]) {
 
-            if (lang === 'sk') {
-              alert("Súbory boli úspešne nahrané!", response);
-            } else {
-              alert("The files were uploaded successfully!", response);
-            }
-          } else {
-            if (lang === 'sk') {
-              alert("Súbory sa nenahrali!");
-            } else {
-              alert("The files were not uploaded!");
-            }
-          }
-        }
-      })
-      .catch(error => console.error("Error:", error));
-  });
+                        if (lang === 'sk') {
+                            alert("Súbory boli úspešne nahrané!", response);
+                        } else {
+                            alert("The files were uploaded successfully!", response);
+                        }
+                    } else {
+                        if (lang === 'sk') {
+                            alert("Súbory sa nenahrali!");
+                        } else {
+                            alert("The files were not uploaded!");
+                        }
+                    }
+                }
+            })
+            .catch(error => console.error("Error:", error));
+    });
 }
